aaa0d138918f2cce237e6e69917762850859acd2
Eslim Rana Emiroglu <rana.emiroglu@ug.bilkent.edu.tr>
2022-05-07 16:07:29 +0300

    Add files via upload

---
+++ CS102_FINAL_PROJECT/Login/T_Shape.java
@@ -0,0 +1,26 @@
+package Login;
+import java.awt.Point;
+
+public class T_Shape extends Tetrade {
+    
+    private final int[][][] T_CORDINATES = 
+    {
+        {{1,0},{0,1},{1,1},{2,1}},
+        {{1,0},{1,1},{2,1},{1,2}},
+        {{0,1},{1,1},{2,1},{1,2}},
+        {{1,0},{0,1},{1,1},{1,2}}
+    };
+
+    public T_Shape(){
+        ID = 5;
+        this.referX = 5;
+        this.referY = 2;
+        this.state = 0;
+        this.blocks[0] = new Point(6,2);
+        this.blocks[1] = new Point(5,3);
+        this.blocks[2] = new Point(6,3);
+        this.blocks[3] = new Point(7,3);
+        this.cordinates = T_CORDINATES;
+    }
+
+}
